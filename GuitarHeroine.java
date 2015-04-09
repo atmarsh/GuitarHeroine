dca0e4d294f26400ddceea77e5c263c508f8e96d
atmarsh <alonzotmarsh@gmail.com>
2015-04-09 12:46:15 -0500

    Update GuitarHeroine.java

--- GuitarHeroine.java
+++ GuitarHeroine.java
@@ -5,55 +5,106 @@
  */
 package GuitarHeroine;
 
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author atmarsh
  */
 public class GuitarString {
-    
+
+    private final int samplingRate = 44100;
+    private RingBuffer buffer;
+    private int ticLog = 0;
+
     /**
-     * Create a guitar string of the given frequency, using a sampling rate of 44,100
+     * Create a guitar string of the given frequency, using a sampling rate of
+     * 44,100
+     *
      * @param frequency Frequency of the guitar string
      */
     public GuitarString(double frequency) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int n = (int) (samplingRate / frequency);
+        buffer = new RingBuffer(n);
+        for (int i = 0; i < n; i++) {
+            try {
+                buffer.enqueue(0);
+            } catch (RingBuffer.BufferFullException ex) {
+                Logger.getLogger(GuitarString.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
-    
+
     /**
-     * Create a guitar string whose size and initial values are given by the array
+     * Create a guitar string whose size and initial values are given by the
+     * array
+     *
      * @param init Array of initial values
      */
-    public GuitarString(double [] init) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public GuitarString(double[] init) {
+        buffer = new RingBuffer(init.length);
+        for (double i : init) {
+            try {
+                buffer.enqueue((int) i);
+            } catch (RingBuffer.BufferFullException ex) {
+                Logger.getLogger(GuitarString.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
-    
+
     /**
      * Sets the buffer to white noise.
      */
     public void pluck() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Random rand = new Random();
+        for (int i = 0; i < buffer.size(); i++) {
+            try {
+                buffer.dequeue();
+            } catch (RingBuffer.BufferEmptyException ex) {
+                Logger.getLogger(GuitarString.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            try {
+                double n = rand.nextDouble() / 2;
+                if(rand.nextBoolean()) {
+                    n *= -1;
+                }
+                buffer.enqueue(n);
+            } catch (RingBuffer.BufferFullException ex) {
+                Logger.getLogger(GuitarString.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
-    
+
     /**
      * Advances the simulation by one time step.
      */
     public void tic() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ticLog++;
     }
-    
+
     /**
      * Returns the current sound sample.
+     *
      * @return current sound sample
      */
     public double sample() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            return buffer.peek();
+        } catch (RingBuffer.BufferEmptyException ex) {
+            Logger.getLogger(GuitarString.class.getName()).log(Level.SEVERE, null, ex);
+            return 0;
+        }
     }
-    
+
     /**
      * Returns the amount of time that has elapsed.
+     *
      * @return the number of tics elapsed.
      */
     public int time() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return ticLog;
     }
 }
