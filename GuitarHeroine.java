f14339c457b5f2b25fdb0e35f3dc81bd97d98bcf
atmarsh <alonzotmarsh@gmail.com>
2015-04-09 12:21:39 -0500

    Create GuitarHeroine.java

---
+++ GuitarHeroine.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GuitarHeroine;
+
+/**
+ *
+ * @author atmarsh
+ */
+public class GuitarString {
+    
+    /**
+     * Create a guitar string of the given frequency, using a sampling rate of 44,100
+     * @param frequency Frequency of the guitar string
+     */
+    public GuitarString(double frequency) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Create a guitar string whose size and initial values are given by the array
+     * @param init Array of initial values
+     */
+    public GuitarString(double [] init) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Sets the buffer to white noise.
+     */
+    public void pluck() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Advances the simulation by one time step.
+     */
+    public void tic() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Returns the current sound sample.
+     * @return current sound sample
+     */
+    public double sample() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Returns the amount of time that has elapsed.
+     * @return the number of tics elapsed.
+     */
+    public int time() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
