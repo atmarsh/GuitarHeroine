38e3ab53f1e8acda782137e86968716336a932fc
atmarsh <alonzotmarsh@gmail.com>
2015-04-10 12:31:03 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GuitarHeroine;
+
+/**
+ *
+ * @author atmarsh
+ */
+public class Player {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GuitarHero gh = new GuitarHero();
+        gh.initialize();
+    }
+    
+}
